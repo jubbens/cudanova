0dc91e5d1f0b0595847c75ff508e327c3a5feaac
Job Diogenes Ribeiro Borges <jobdiogenes@gmail.com>
2020-02-05 17:34:14 -0300

    bug fix

--- setup.py
+++ setup.py
@@ -1,26 +1,8 @@
-import os
-import sys
 from setuptools import setup, find_packages
-
-def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    # intentionally *not* adding an encoding option to open, See:
-    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
-    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
-        return fp.read()
-    
-def get_version(rel_path):
-    for line in read(rel_path).splitlines():
-        if line.startswith('__version__'):
-            # __version__ = "0.9"
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    else:
-        raise RuntimeError("Unable to find version string.")
     
 setup(
     name='cudanova',
-    version=get_version("src/__init__.py"),
+    version='0.1.0',
     author='Jordan Ubbens',
     author_email='jubbens@gmail.com',
     description='GPU accelerated implementation of permutational multivariate analysis of variance (PERMANOVA).',
